775d3eeb09f120047fc7b528a5bd03987090e14b
Abhishek Singh <singh4758@gmail.com>
2023-06-04 08:09:29 +0530

    fix: change title of swagger

--- src/libs/swagger.ts
+++ src/libs/swagger.ts
@@ -5,9 +5,9 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'User API',
+      title: 'Hobbies Tracker API',
       version: '1.0.0',
-      description: 'API documentation for the User API',
+      description: 'API documentation for the Hobbies and User API',
     },
     servers: [
       {
